b8857a08f737fd809adc3f399d7e5a89de7e04c8
Kaustubh Shankar <kaustubhshan@gmail.com>
2021-06-14 14:04:39 +0530

    mmap() args modified

--- mem_mang/src/mm.c
+++ mem_mang/src/mm.c
@@ -13,27 +13,30 @@ void mm_init() {
 
 /* To request VM page from the kernel */
 static void *request_vm_page(uint32_t units) {
-    char *vm_page = mmap(0, units*SYSTEM_PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANON|MAP_PRIVATE, 0, 0);
+    char *vm_page = mmap(sbrk(0), units*SYSTEM_PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANON|MAP_PRIVATE, -1, 0);
     if(vm_page == MAP_FAILED){
         printf("Error : VM page allocation failed\n");
         return NULL;
     }
-    memset(vm_page, 0, units * SYSTEM_PAGE_SIZE);
+    memset(vm_page, 0, units*SYSTEM_PAGE_SIZE);
     return (void *)vm_page;
 }
 
 /* To release VM page to the kernel */
 static void release_vm_page(void *vm_page, uint32_t units) {
-    if(munmap(vm_page, units * SYSTEM_PAGE_SIZE)){
+    if(munmap(vm_page, units*SYSTEM_PAGE_SIZE)){
         printf("Error : Could not release VM page to kernel\n");
     }
 }
 
+/*
 int main() {
     mm_init();
     printf("VM page size = %lu\n", SYSTEM_PAGE_SIZE);
     void *addr1 = request_vm_page(1);
     void *addr2 = request_vm_page(1);
     printf("addr1 = %p --- addr2 = %p\n", addr1, addr2);
+    printf("Diff = %u\n", (uint32_t)(addr2 - addr1));
     return 0;
 }
+*/
