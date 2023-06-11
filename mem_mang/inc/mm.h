58a3d171915dacebb38005e91ab0d14c9ca715c8
kaustubhshan27 <kaustubhshan@gmail.com>
2023-06-11 18:32:08 +0530

    revised mm unit

--- mem_mang/inc/mm.h
+++ mem_mang/inc/mm.h
@@ -1,9 +1,10 @@
 #ifndef _MEM_MANG_
 #define _MEM_MANG_
 
-#include <stdlib.h>
-#include <stdbool.h>
-
-
+#include <stdio.h>
+#include <stdint.h>
+#include <memory.h>
+#include <unistd.h>     /* for getpagesize() */
+#include <sys/mman.h>   /* for mmap() and munmap() */
 
 #endif /* _MEM_MANG_ */
--- mem_mang/src/mm.c
+++ mem_mang/src/mm.c
@@ -1,31 +1,29 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <memory.h>
-#include <unistd.h>     /* for getpagesize */
-#include <sys/mman.h>   /* for mmap() */
+#include "../inc/mm.h"
 
 /* size of VM page on system */
 static size_t SYSTEM_PAGE_SIZE = 0;
 
 /* to initialize the memory manager */
-void mm_init() {
+void mm_init(void) {
     SYSTEM_PAGE_SIZE = getpagesize();
 }
 
 /* to request VM page from the kernel */
 static void *request_vm_page(uint32_t units) {
-    char *vm_page = mmap(sbrk(0), units*SYSTEM_PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+    /* the virtual mapping should be done in the heap */
+    uint8_t *vm_page = mmap(sbrk(0), units*SYSTEM_PAGE_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
     if(vm_page == MAP_FAILED){
-        printf("Error : VM page allocation failed\n");
+        printf("Error: VM page allocation failed\n");
         return NULL;
     }
     memset(vm_page, 0, units*SYSTEM_PAGE_SIZE);
+
     return (void *)vm_page;
 }
 
 /* to release VM page to the kernel */
 static void release_vm_page(void *vm_page, uint32_t units) {
     if(munmap(vm_page, units*SYSTEM_PAGE_SIZE)){
-        printf("Error : Could not release VM page to kernel\n");
+        printf("Error: Could not release VM page to kernel\n");
     }
 }
