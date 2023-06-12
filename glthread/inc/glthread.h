c8a383691281aea54b9777dadb80652a662c0ce8
kaustubhshan27 <kaustubhshan@gmail.com>
2023-06-12 20:24:55 +0530

    added descriptions for glthread apis

--- glthread/inc/glthread.h
+++ glthread/inc/glthread.h
@@ -15,10 +15,15 @@ typedef struct glthread {
     unsigned int offset;
 } glthread_t;
 
+/* initializes a glthread */
 void glthread_init(glthread_t *list, uint32_t offset);
+/* initializes a glthread node */
 void glthread_init_node(glthread_node_t *glthread_node);
+/* adds a node to a glthread */
 void glthread_add_node(glthread_t *list, glthread_node_t *node);
+/* removes a node from a glthread */
 void glthread_remove_node(glthread_t *list, glthread_node_t *node);
+/* deletes the entire glthread */
 void glthread_delete(glthread_t *list);
 
 #define OFFSETOF(struct_type, field_name)   \
