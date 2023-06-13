e17635c28e2bdfc7b0f2cc40388026d1ad9a4b4a
kaustubhshan27 <kaustubhshan@gmail.com>
2023-06-13 17:27:06 +0530

    created a test application to verify LMM

---
+++ test/test_app.c
@@ -0,0 +1,24 @@
+#include "../mem_mang/inc/uapi_mm.h"
+
+typedef struct emp {
+    char name[32];
+    uint32_t emp_id;
+} empt_t;
+
+typedef struct student {
+    char name[32];
+    uint32_t stu_id;
+    char degree[24];
+    uint8_t grade;
+} student_t;
+
+int main(int argc, char **argv) {
+    mm_init();
+    
+    MM_REG_STRUCT(empt_t);
+    MM_REG_STRUCT(student_t);
+    
+    mm_print_registered_struct_records();
+    
+    return 0;
+}
